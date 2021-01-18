e5f00189c6dc56626b7074716552e316f0d02649
TurcnaninovUV <pro-h92@yandex.ru>
2021-01-18 17:49:44 +0300

    Исправил замечания

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,24 @@
 fun main() {
-    usualUser(2000)
+    usualUser(11000)
     musicLover(6000)
 
 }
 
 fun usualUser(buy: Int) {
-    if (buy < 1000) println("Сумма вашей покупки равна $buy")
-    if (buy > 1000) println("Сумма вашей покупки с учетом скидки равна ${buy - 100}")
-    if (buy > 10000) println("Сумма вашей покупки с учетом скидки равна ${buy * 0.95}")
+    var lastPurchase = 1001
+    if (lastPurchase in 0..1000) println("Сумма вашей покупки равна $buy рублей")
+    if (lastPurchase in 1001..10000) println("Сумма вашей покупки с учетом скидки равна ${buy - 100} рублей")
+    if (lastPurchase > 10000) println("Сумма вашей покупки с учетом скидки равна ${buy * 0.95} рублей")
 }
 
 fun musicLover(buyMusicLover: Int) {
+    var lastPurchaseMusicLover = 5000
     val discount = 0.99
-    if (buyMusicLover < 1000) println("Сумма вашей покупки равна ${buyMusicLover * discount}")
-    if (buyMusicLover > 1000) println("Сумма вашей покупки с учетом скидки равна ${(buyMusicLover - 100) * discount}")
-    if (buyMusicLover > 10000) println("Сумма вашей покупки с учетом скидки равна ${(buyMusicLover * 0.95) * discount}")
+    if (lastPurchaseMusicLover in 0..1000) println("Сумма вашей покупки равна ${buyMusicLover * discount} рублей")
+    if (lastPurchaseMusicLover in 1001..10000) println("Сумма вашей покупки с " +
+            "учетом скидки равна ${(buyMusicLover - 100) * discount} рублей")
+    if (lastPurchaseMusicLover > 10000) println("Сумма вашей покупки " +
+            "с учетом скидки равна ${(buyMusicLover * 0.95) * discount} рублей")
 }
 
 
